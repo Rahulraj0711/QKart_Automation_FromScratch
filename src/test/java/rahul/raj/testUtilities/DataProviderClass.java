91959d4de5ab35c68abeab53edc18544baf3229e
Rahul Raj <justrahulronaldo1999@gmail.com>
2025-06-02 00:10:25 +0530

    Updated DataProviderClass

--- src/test/java/rahul/raj/testUtilities/DataProviderClass.java
+++ src/test/java/rahul/raj/testUtilities/DataProviderClass.java
@@ -1,6 +1,5 @@
 package rahul.raj.testUtilities;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
@@ -22,16 +21,14 @@ public class DataProviderClass {
     public Object[][] dpMethod(Method m) throws IOException {
         int rowIndex = 0;
         int cellIndex = 0;
-        List<List<?>> outputList = new ArrayList<List<?>>();
+        List<List<?>> outputList = new ArrayList<>();
 
-        FileInputStream excelFile = new FileInputStream(new File(System.getProperty("user.dir")+"/src/test/java/rahul/raj/resources/DataFile.xlsx"));
+        FileInputStream excelFile = new FileInputStream(System.getProperty("user.dir")+"/src/test/java/rahul/raj/resources/DataFile.xlsx");
         Workbook workbook = new XSSFWorkbook(excelFile);
         Sheet selectedSheet = workbook.getSheet(m.getName());
-        Iterator<Row> iterator = selectedSheet.iterator();
-        while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
-            List<String> innerList = new ArrayList<String>();
+        for (Row row : selectedSheet) {
+            Iterator<Cell> cellIterator = row.cellIterator();
+            List<String> innerList = new ArrayList<>();
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
                 if (rowIndex > 0 && cellIndex > 0) {
@@ -45,16 +42,12 @@ public class DataProviderClass {
             }
             rowIndex = rowIndex + 1;
             cellIndex = 0;
-            if (innerList.size() > 0)
+            if (!innerList.isEmpty())
                 outputList.add(innerList);
-
         }
-
         excelFile.close();
         workbook.close();
 
-        String[][] stringArray = outputList.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
-        return stringArray;
-
+        return outputList.stream().map(u -> u.toArray(new Object[0])).toArray(Object[][]::new);
     }
 }
